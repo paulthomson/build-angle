02038a05301b54b31f586c2677eaec0c7cdfbeb6
Paul Thomson <paulthomson@google.com>
2018-10-18 09:30:06 +0100

    Update travis build scripts.

--- 1-install-deps-travis.sh
+++ 1-install-deps-travis.sh
@@ -23,8 +23,26 @@ then
   GITHUB_RELEASE_TOOL_ARCH="linux_amd64"
 fi
 
+pushd /data
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+cd depot_tools
+git checkout 08faab99d41bd4f1f727267586ff28a227f80cd3
+popd
+
 pushd /data/bin
 wget "https://github.com/${GITHUB_RELEASE_TOOL_USER}/github-release/releases/download/${GITHUB_RELEASE_TOOL_VERSION}/github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
 tar xf "github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
 popd
 
+export PATH=/data/depot_tools:$PATH
+
+git clone https://chromium.googlesource.com/angle/angle "${CLONE_DIR}"
+cd "${CLONE_DIR}"
+git checkout $(cat ../COMMIT_ID)
+python scripts/bootstrap.py
+gclient sync
+
+if [ "$(uname)" == "Linux" ];
+then
+  ./build/install-build-deps.sh
+fi
